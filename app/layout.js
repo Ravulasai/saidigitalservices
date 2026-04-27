112b534a054a2c38a60f7740eb10576e26afa9ab
Ravulasai <sairavula1919@gmail.com>
2026-04-27 14:58:55 +0530

    feat: add Google Analytics G-HJVSD7LB1F

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Cormorant_Garamond, Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import Footer from '@/components/Footer';
@@ -169,6 +170,19 @@ export default function RootLayout({ children }) {
           <main id="main-content">{children}</main>
           <Footer />
         </ThemeProvider>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HJVSD7LB1F"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HJVSD7LB1F');
+          `}
+        </Script>
       </body>
     </html>
   );
